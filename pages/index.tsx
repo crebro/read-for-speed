fe03f6be5803397bb389e7198e47a52fbb1c0d37
crebro <creationduwal@gmail.com>
2022-02-10 22:56:05 +0545

    fix: remove bugs and add button for toggling reading

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 const Home: NextPage = () => {
   const [contents, setContents] = useState<string>('');
   const [wordIndex, setWordIndex] = useState(0);
   const [wordList, setWordList] = useState<string[]>(["Hello!"]) ;
   const [reading, setReading] = useState(false);
+  const [playingInterval, setPlayingInterval] = useState<NodeJS.Timer | null>();
 
   const getWordList = (contents: string) => {
     setWordList(contents.split(' '));
@@ -17,18 +18,31 @@ const Home: NextPage = () => {
     setReading(true);
   }
 
+  const stopReading = () => {
+    if (playingInterval) {
+      window.clearInterval(playingInterval);
+      setReading(false);
+      setWordIndex(0);
+      setPlayingInterval(null);
+    }
+  }
+
   useEffect(() => {
-    console.log("this is getting called outside of the thing");
-    const interval = setInterval(() => {
-      setWordIndex((prevIndex) => {
-        if (prevIndex >= wordList.length - 1) {
-          window.clearInterval(interval);
-          setReading(false);
-          console.log("this is getting called to stop the operation");
-        }
-        return prevIndex + 1
-      });
-    }, 150);
+    if (reading) {
+      const interval = setInterval(() => {
+        console.log('I am wondienrg');
+        setWordIndex((prevIndex) => {
+          if (prevIndex >= wordList.length - 1) {
+            window.clearInterval(interval);
+            setReading(false);
+            setWordIndex(0);
+            setPlayingInterval(null);
+          }
+          return prevIndex + 1
+        });
+      }, 150);
+      setPlayingInterval(interval);
+    }
   }, [reading])
   
   return (
@@ -36,17 +50,17 @@ const Home: NextPage = () => {
       <Head>
         <title> Read for speed, read documents like pro, without spending a minute more </title>
       </Head>
-      <div className='w-[50%]'>
+      <div className='w-[50%] transition duration-150'>
         <div className='text-2xl font-bold'> Read-For-Speed </div>
         <div className='text-xl '> Read documents like a pro, without spending a minute more</div>
         {
           !reading ? 
-          <textarea className='w-full py-2 px-2 border-1 bg-gray-300 rounded-lg border-red-100 outline-1 outline-blue-700' onChange={(e) => setContents(e.currentTarget.value)} rows={10} defaultValue={"Type your content here"} />
-          : <div className='text-3xl flex items-center justify-center'>
+          <textarea className='w-full py-2 px-2 border-1 bg-gray-300 rounded-lg border-red-100 outline-1 outline-blue-700' onChange={(e) => setContents(e.currentTarget.value)} rows={10} defaultValue={contents} />
+          : <div className='font-bold my-10 text-3xl flex items-center justify-center'>
             { wordList[wordIndex] }
            </div>
         }
-        <button onClick={() => startReading()} className='bg-red-500 my-2 text-white px-4 py-2 w-full'> Start Reading </button>
+        <button onClick={() =>  !reading ? startReading() : stopReading()} className={`${ reading ? 'bg-red-400' : 'bg-indigo-500'} transition duration-150 my-2 text-white px-4 py-2 w-full`}> { reading ? "Stop Reading" : "Start Reading" } </button>
       </div>
     </div>
   )
